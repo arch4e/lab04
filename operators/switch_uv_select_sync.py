cd9177ae2795c840fc78f7737c92d2948c5ace4b
ShioN <shion@arch4e.com>
2023-03-11 15:59:26 +0900

    update: change the loop selection state so that it is inherited before and after switching uv select sync

--- operators/switch_uv_select_sync.py
+++ operators/switch_uv_select_sync.py
@@ -1,4 +1,5 @@
 import bpy
+import bmesh
 
 from .register import registerdcr
 
@@ -8,13 +9,92 @@ class SwitchUVSelectSync(bpy.types.Operator):
     bl_label  = "switch uv select sync"
 
     def execute(self, context):
-        current_flag = bpy.context.scene.tool_settings.use_uv_select_sync
         try:
-            if current_flag == True:
+            backup_data = []
+
+            if bpy.context.scene.tool_settings.use_uv_select_sync:
+                # backup selected loops
+                bpy.ops.object.mode_set(mode='EDIT')
+                backup_data = backup_selected_loops_from_bmesh()
+
+                # select all vertex
+                bpy.ops.object.mode_set(mode='EDIT')
                 bpy.ops.mesh.select_all(action="SELECT")
-            bpy.context.scene.tool_settings.use_uv_select_sync = not current_flag
+
+                # disable uv select sync
+                bpy.context.scene.tool_settings.use_uv_select_sync = False
+
+                # restore selected loops
+                # ! getting uv_layers.active.data only works in object mode
+                bpy.ops.object.mode_set(mode='OBJECT')
+                restore_selected_loops_to_uv_layer(backup_data)
+                bpy.ops.object.mode_set(mode='EDIT')
+            elif not bpy.context.scene.tool_settings.use_uv_select_sync:
+                # backup selected loops
+                # ! getting uv_layers.active.data only works in object mode
+                bpy.ops.object.mode_set(mode='OBJECT')
+                backup_data = backup_selected_loops_from_uv_layer()
+
+                # enable uv select sync
+                bpy.context.scene.tool_settings.use_uv_select_sync = True
+
+                # restore selected loops
+                bpy.ops.object.mode_set(mode='EDIT')
+                restore_selected_loops_to_bmesh(backup_data)
 
             return {'FINISHED'}
         except Exception as e:
             print(e)
             return { "CANCELLED" }
+
+def backup_selected_loops_from_bmesh():
+    backup_data = []
+    for i in range(len(bpy.context.selected_objects)):
+        backup_data.append([])
+        obj = bpy.context.selected_objects[i]
+        bm = bmesh.new()
+        bm = bmesh.from_edit_mesh(obj.data)
+        loops = [loop for face in bm.faces for loop in face.loops]
+
+        for l in loops:
+            backup_data[i].append(l.vert.select)
+
+    return backup_data
+
+def backup_selected_loops_from_uv_layer():
+    backup_data = []
+    for i in range(len(bpy.context.selected_objects)):
+        backup_data.append([])
+        obj = bpy.context.selected_objects[i]
+
+        for j in range(len(obj.data.uv_layers.active.data.items())):
+            backup_data[i].append(obj.data.uv_layers.active.data[j].select)
+
+    return backup_data
+
+def restore_selected_loops_to_bmesh(source_data):
+    for i in range(len(bpy.context.selected_objects)):
+        obj = bpy.context.selected_objects[i]
+        bm = bmesh.new()
+        bm = bmesh.from_edit_mesh(obj.data)
+        loops = [loop for face in bm.faces for loop in face.loops]
+
+        # deselect loops
+        for j in range(len(loops)):
+            loops[j].vert.select_set(False)
+
+        # only the loop selected in the uv layer are made selected
+        # ! because when a loop on the same vertex is deselected,
+        #   the previously selected loop is deselected
+        for j in range(len(loops)):
+            if source_data[i][j]:
+                loops[j].vert.select_set(True)
+
+        bm.select_flush_mode()
+
+def restore_selected_loops_to_uv_layer(source_data):
+    for i in range(len(bpy.context.selected_objects)):
+        obj = bpy.context.selected_objects[i]
+
+        for j in range(len(obj.data.uv_layers.active.data.items())):
+            obj.data.uv_layers.active.data[j].select = source_data[i][j]
