954c5b0c912453cb9b5fabae2370eef7b844a448
ShioN <shion@arch4e.com>
2023-03-13 23:25:47 +0900

    feat: add operators for shortcut registration

---
+++ operators/instant_sc.py
@@ -0,0 +1,54 @@
+import bpy
+
+from .register import registerdcr
+
+@registerdcr
+class InstantSC_1(bpy.types.Operator):
+    bl_idname = "lab04.instant_sc_1"
+    bl_label  = "instant short cut - 1"
+
+    def execute(self, context):
+        try:
+            instant_sc_1()
+            return {'FINISHED'}
+        except Exception as e:
+            print(e)
+            return { "CANCELLED" }
+
+@registerdcr
+class InstantSC_2(bpy.types.Operator):
+    bl_idname = "lab04.instant_sc_2"
+    bl_label  = "instant short cut - 2"
+
+    def execute(self, context):
+        try:
+            instant_sc_2()
+            return {'FINISHED'}
+        except Exception as e:
+            print(e)
+            return { "CANCELLED" }
+
+@registerdcr
+class InstantSC_3(bpy.types.Operator):
+    bl_idname = "lab04.instant_sc_3"
+    bl_label  = "instant short cut - 3"
+
+    def execute(self, context):
+        try:
+            instant_sc_3()
+            return {'FINISHED'}
+        except Exception as e:
+            print(e)
+            return { "CANCELLED" }
+
+def instant_sc_1():
+    print("instant_sc_1")
+    # edit here
+
+def instant_sc_2():
+    print("instant_sc_2")
+    # edit here
+
+def instant_sc_3():
+    print("instant_sc_3")
+    # edit here
